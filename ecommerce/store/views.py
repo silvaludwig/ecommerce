a6b094e036f881f16a4929fcc4ac2dcef780b423
Ludwig S <silvaludwig@outlook.com>
2025-06-08 08:11:52 -0300

    base templates, urls in homepage and about page

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -4,3 +4,6 @@ from .models import Product
 def home(request):
     products = Product.objects.all() #query into database 
     return render(request, 'home.html', {'products': products})
+
+def about(request):
+    return render(request, 'about.html', {})
