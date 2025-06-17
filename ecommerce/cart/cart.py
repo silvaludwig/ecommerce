506eaa5a7310feb8322160333b42b5140c516846
Ludwig S <silvaludwig@outlook.com>
2025-06-17 13:09:54 -0300

    refactor cart functionality and update cart summary template

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -1,3 +1,5 @@
+from store.models import Product
+
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -22,3 +24,8 @@ class Cart():
 
     def __len__(self):
         return len(self.cart)
+    
+    def get_prods(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        return products
